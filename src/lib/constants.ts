66e1b45798bf1b55f2e8e85f48c9138fa661083c
codenamemoe <newmoeali@gmail.com>
2024-11-07 09:06:34 +0000

    chore: updated constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -18,5 +18,19 @@ export const SAMPLE_QUESTION: QuizQuestion = {
         { id: 'opt4', label: 'Chloroplast', isCorrect: false },
       ],
     },
+    {
+        id: 'g3',
+        options: [
+          { id: 'opt5', label: 'Partially permeable membrane', isCorrect: true },
+          { id: 'opt6', label: 'Impermeable membrane', isCorrect: false },
+        ],
+      },
+      {
+        id: 'g4',
+        options: [
+          { id: 'opt7', label: 'Cellulose', isCorrect: false },
+          { id: 'opt8', label: 'Mitochondria', isCorrect: true },
+        ],
+      },
   ],
 };
